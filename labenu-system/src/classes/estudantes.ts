897d28e46295ca51f9515fba83605655233f1442
Simone Xavier <jerusa.xaviers@gmail.com>
2022-06-22 16:09:11 -0300

    inicio

---
+++ labenu-system/src/classes/estudantes.ts
@@ -0,0 +1,18 @@
+import { Usuarios } from "./usuarios";
+
+export class Estudantes extends Usuarios{
+    hobbies: string[]
+
+constructor(
+    id: string,
+    nome: string,
+    email: string,
+    data_nasc: string,
+    turma_id: string,
+    hobbies: string[]
+    ){
+    super(id,nome, email, data_nasc, turma_id)
+    this.hobbies = hobbies
+}
+
+}
